069018f22f18eb14f3d6f8a559012f65f87262eb
Juana López <juana.lopez@correo.ucu.edu.uy>
2023-11-01 14:18:37 -0300

    entrega lista creo

---
+++ src/Library/ITextToPrint.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+namespace Full_GRASP_And_SOLID
+{
+    public interface ITextToPrint
+    {
+        string GetTextToPrint();
+    }
+
+}
---
+++ tests/Library.Tests/RecipeTests.cs
@@ -0,0 +1,25 @@
+using Full_GRASP_And_SOLID;
+
+namespace Library.Tests;
+
+public class RecipeTests
+{
+    [Test]
+    public void CorrectGetProductionCost()
+    {
+        Product myProduct = new Product("café", 55.0);
+        Equipment myEquipment = new Equipment("batidora", 80.0);
+        Product myProduct2 = new Product("leche", 23.0);
+        Equipment myEquipment2 = new Equipment("", 0);
+
+        Recipe myRecipe = new Recipe();
+        myRecipe.AddStep(myProduct, 2.0, myEquipment, 2);
+        myRecipe.AddStep(myProduct2, 1.0, myEquipment2, 0);
+
+        double expected = 293;
+        double myResult = myRecipe.GetProductionCost();
+
+        Assert.That(myResult, Is.EqualTo(expected));
+    }
+
+}
